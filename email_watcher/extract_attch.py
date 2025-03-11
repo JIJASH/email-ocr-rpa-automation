25622352f446a053fb9199067b01f24c39fd7913
biplavdotcom <lostking05@gmail.com>
2025-03-11 18:18:42 +0545

    save to drive

--- email_watcher/extract_attch.py
+++ email_watcher/extract_attch.py
@@ -6,12 +6,41 @@ from email import message_from_bytes
 from email.utils import parseaddr
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google.oauth2 import service_account
 
 email = "v6053724@gmail.com"
 password = "tfou vyfa vhhh qskl"
 imapServer = "imap.gmail.com"
-attachment_dir = "./attachments"
+attachment_dir = "./temp"
 os.makedirs(attachment_dir, exist_ok=True)
+service_acc_cred = "./serviceAcc/mail-watcher-47-49ce629e4ae7.json"
+scopes = ["https://www.googleapis.com/auth/drive.file"]
+
+credentials = service_account.Credentials.from_service_account_file(
+    service_acc_cred, scopes=scopes
+)
+drive_service = build("drive", "v3", credentials = credentials)
+
+
+folder_id = "1J7bYwFbs-5R80Xg5nWxsakMBtbefUKCs" # this is from the drive folder where i am saving
+
+def upload_to_drive(file_path):
+    """Uploads file to Google Drive"""
+    file_metadata = {"name": os.path.basename(file_path), "parents": [folder_id]}
+    media = MediaFileUpload(file_path, resumable=True)
+    
+    file = drive_service.files().create(
+        body=file_metadata,
+        media_body=media,
+        fields="id"
+    ).execute()
+    
+    # print(f" Uploaded to Google Drive: {file.get('id')}")
+    print(f"Attachment uploaded to Google Drive!")
+
+    return file.get("id")
 
 def extract_details(msg):
   sender_name, sender_email = parseaddr(msg['From'])
@@ -23,7 +52,7 @@ def extract_details(msg):
   print(f"Email address: {sender_email}")
   print(f"Recieved on: {date} \n")
   print(f"Subject: {subject} \n")
-  
+
   if msg.is_multipart():
             for part in msg.walk():
                 content_type = part.get_content_type()
@@ -38,12 +67,14 @@ def extract_details(msg):
     body_preview = body if body else "No body content found"
   print(f"📜 Body: {body_preview}")
   for part in msg.walk():
-          if part.get_filename():  
+          if part.get_filename():
               filename = part.get_filename()
               file_path = os.path.join(attachment_dir, filename)
               with open(file_path, "wb") as f:
                   f.write(part.get_payload(decode=True))
-              print(f"📥 Attachment downloaded: {filename} \n")
+              # print(f"Attachment downloaded: {filename} \n")
+              drive_file_id = upload_to_drive(file_path)          # upload
+              os.remove(file_path)      # remove the local temp file
               time.sleep(5)
               send_reply(sender_email)
   print("-----")
@@ -71,8 +102,7 @@ def send_reply(to_email):
       print("-------------------------")
   except Exception as e:
       print(f"Error sending reply to {to_email}: {e}")
-
-print('Reply sent successfully!')
+  
 def check_for_attachments():
     print("Checking emails")
     client = IMAPClient(imapServer)
@@ -80,9 +110,9 @@ def check_for_attachments():
     client.select_folder('INBOX')
     while True:
           try:
-              client.idle()  
+              client.idle()
               # print("Entering idle mode")
-              responses = client.idle_check(timeout=300)  
+              responses = client.idle_check(timeout=300)
               client.idle_done()
               # print("Exiting idle mode")
               if responses:
@@ -90,7 +120,7 @@ def check_for_attachments():
                 if messages:
                   print("New email recieved! ")
                   for msg_id in messages:
-                    raw_email = client.fetch(msg_id, ['RFC822'])[msg_id][b'RFC822']  
+                    raw_email = client.fetch(msg_id, ['RFC822'])[msg_id][b'RFC822']
                     processed_mail = message_from_bytes(raw_email)
                   # print(processed_mail)
                     extract_details(processed_mail)
